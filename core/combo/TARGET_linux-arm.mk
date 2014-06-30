cd93d5f71198242a336f678db02253145f91906b
JustArchi <JustArchi@JustArchi.net>
2014-06-30 19:08:08 +0200

    Revert "Here we come, 4.9"
    This reverts commit b6d99cefe29f8880e898ce925d376093bc13a3a2.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -42,10 +42,9 @@ endif
 
 # This specifies toolchain being used. In case of issues, stick with 4.7
 # 4.8 is default in my optimizations, as it gives noticable performance boost
-# Don't use 4.9 unless you feel confident, varios strange libstlport segmentation faults may happen (bootloop)
 ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
 #TARGET_GCC_VERSION := 4.7
-TARGET_GCC_VERSION := 4.9
+TARGET_GCC_VERSION := 4.8
 else
 TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
